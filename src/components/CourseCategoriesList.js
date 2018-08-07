7a984fda959dc98a4aa75e94e00177dbc4c46525
Kellen Mace <kellen.mace@gmail.com>
2018-08-06 23:04:54 -0400

    Add course searching

--- src/components/CourseCategoriesList.js
+++ src/components/CourseCategoriesList.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import CourseCategoryCard from '../components/CourseCategoryCard';
 
-const CourseCategoriesList = props => {
-  if ( ! props.courseCategories || ! props.courseCategories.length ) return <p>No course categories found :(</p>;
+class CourseCategoriesList extends Component {
+  state = {
+    search: ''
+  };
 
-  const courseCategories = props.courseCategories.map( index => index.courseCategory );
+  handleSearch = event => {
+    this.setState({ search: event.target.value });
+  }
 
-  return (
-    <section className="course-cat-list-container">
-      <ul className="course-cat-list">
-        {courseCategories.map( courseCategory => (
-          <CourseCategoryCard key={ courseCategory.id } courseCategory={ courseCategory } />
-        ) )}
-      </ul>
-    </section>
-  );
+  getVisibleCategories = () => {
+    const { courseCategories } = this.props;
+    const { search } = this.state;
+    if (!courseCategories || !courseCategories.length) return [];
+    const categories = courseCategories.map(index => index.courseCategory);
+    if (!search) return categories;
+    return categories.filter(category => category.name.toLowerCase().includes(search.toLowerCase()));
+  }
+
+  render() {
+    const categoriesVisible = this.getVisibleCategories();
+
+    return (
+      <section className="course-cat-list-container">
+        <input
+          type="text"
+          placeholder="Language / framework"
+          name="search"
+          onChange={this.handleSearch}
+          value={this.state.search}
+        />
+        { categoriesVisible && categoriesVisible.length ?
+          <ul className="course-cat-list">
+            {categoriesVisible.map( courseCategory => (
+              <CourseCategoryCard key={ courseCategory.id } courseCategory={ courseCategory } />
+            ) )}
+          </ul>
+        :
+          <p>No course categories found :(</p>
+        }
+      </section>
+    );
+  }
 }
 
 const GET_COURSE_CATEGORIES = gql`
